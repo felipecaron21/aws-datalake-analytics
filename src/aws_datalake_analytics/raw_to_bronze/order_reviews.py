8edf5e872abfee21a7a23af2106166cbaf30f197
Felipe Caron <felipe.caronap@outlook.com>
2026-07-24 20:50:11 -0300

    add: order_reviews raw_to_bronze script

---
+++ src/aws_datalake_analytics/raw_to_bronze/order_reviews.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from aws_datalake_analytics.utils.s3_helpers import (
+    criar_cliente_s3,
+    ler_csv_do_s3,
+    escrever_parquet_no_s3,
+)
+
+bucket_name = "aws-datalake-analytics-felipecaron"
+raw_key = "raw/olist_order_reviews_dataset.csv"
+bronze_key = "bronze/order_reviews/order_reviews.parquet"
+
+cliente_s3 = criar_cliente_s3()
+
+df_order_reviews = ler_csv_do_s3(cliente_s3, bucket_name, raw_key)
+
+string_columns = [
+    "review_id",
+    "order_id",
+    "review_comment_title",
+    "review_comment_message",
+]
+
+int_columns = [
+    "review_score",
+]
+
+date_columns = [
+    "review_creation_date",
+    "review_answer_timestamp",
+]
+
+try:
+    df_order_reviews[string_columns] = df_order_reviews[string_columns].astype("string")
+    df_order_reviews[int_columns] = df_order_reviews[int_columns].astype("int")
+    df_order_reviews[date_columns] = df_order_reviews[date_columns].apply(
+        pd.to_datetime
+    )
+except Exception as erro:
+    print(f"Erro ao aplicar tipagem nas colunas de order_reviews: {erro}")
+    raise
+
+escrever_parquet_no_s3(cliente_s3, df_order_reviews, bucket_name, bronze_key)
