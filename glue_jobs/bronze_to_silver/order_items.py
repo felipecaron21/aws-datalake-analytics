7feb84c40394481145103361eb2307ae9cbd657d
Felipe Caron <felipe.caronap@outlook.com>
2026-07-28 13:00:50 -0300

    add: order_items bronze_to_silver Glue Job script

---
+++ glue_jobs/bronze_to_silver/order_items.py
@@ -0,0 +1,20 @@
+import sys
+import pandas as pd
+from awsglue.utils import getResolvedOptions
+from s3_helpers import (
+    criar_cliente_s3,
+    ler_parquet_do_s3,
+    escrever_parquet_no_s3,
+)
+
+args = getResolvedOptions(sys.argv, ["bucket_name", "bronze_key", "silver_key"])
+
+bucket_name = args["bucket_name"]
+bronze_key = args["bronze_key"]
+silver_key = args["silver_key"]
+
+cliente_s3 = criar_cliente_s3()
+
+df_order_items = ler_parquet_do_s3(cliente_s3, bucket_name, bronze_key)
+
+escrever_parquet_no_s3(cliente_s3, df_order_items, bucket_name, silver_key)
